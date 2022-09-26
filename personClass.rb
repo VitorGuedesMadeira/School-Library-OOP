60c9d4f57ea0d7ec05e171d183952b5d7394aa36
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-09-26 20:28:03 -0300

    Fix linters

--- personClass.rb
+++ personClass.rb
@@ -3,35 +3,38 @@ class Person
   attr_reader :id
 
   def initialize(name = "Unknown", age = true, parent_permission)
-    @id = 
-    @name = name
+    @id =
+      @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-#id getter
+  # id getter
   def id
     @id
-  end 
-#name getter
+  end
+
+  # name getter
   def name
     @name
-  end 
-#age getter
+  end
+
+  # age getter
   def age
     @age
-  end 
+  end
 
-#name setter
+  # name setter
   def setname=(name)
     @name = name
   end
-#age setter
+
+  # age setter
   def setage=(age)
     @age = age
   end
 
-#private methods
+  # private methods
   def is_of_age?
     @age >= 18 ? true : false
   end
