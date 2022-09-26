c1aa8dc0d162526ec56c2e9d177e874c045829b4
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-09-26 19:12:17 -0300

    Add teacherClass file

---
+++ teacherClass.rb
@@ -0,0 +1,13 @@
+require_relative 'personClass'
+
+class Teacher < Person
+    attr_accessor :specialization
+    
+    def initialize(specialization)
+      @specialization = specialization
+    end
+  
+    def can_use_services?
+      return true
+    end
+  end
