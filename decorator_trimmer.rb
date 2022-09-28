c9b5d6e272b2dd8483450f8e5b6ed00cf478edbf
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-09-28 16:20:00 -0300

    Change changes required

--- decorator_trimmer.rb
+++ decorator_trimmer.rb
@@ -2,6 +2,6 @@ require_relative './decorator_base'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    @nameable.correct_name[0..9]
+    @nameable.correct_name.length > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name
   end
 end
--- nameable_class.rb
+++ nameable_class.rb
@@ -1,5 +1,5 @@
 class Nameable
   def correct_name
-    raise NotImplementedError
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
   end
 end
